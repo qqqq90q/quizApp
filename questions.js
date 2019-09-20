4287b487206e75cf03e380f7219db62164857c73
qqqq90q <34322694+qqqq90q@users.noreply.github.com>
2019-09-20 09:30:53 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,52 @@
+//our question database
+let STORE = [{
+  question: 'What is the state capitol of Maine?',
+  answers: ['Bangor','Portland','Augusta','StockBridge'],
+  correctAnswer: 'Augusta'
+},
+{
+  question: 'What is the state capitol of Michigan?',
+  answers: ['Lansing','Detroit','SouthField','Royal Oak'],
+  correctAnswer: 'Lansing'
+},
+{
+  question: 'What is the state capitol of Florida?',
+  answers: ['Talahasee','Saint PetersBerg','Miami','ClearWater'],
+  correctAnswer: 'Talahasee'
+},
+{
+  question: 'What is the state capitol of California?',
+  answers: ['Los Angeles','San Fransisco','San Diego', 'Sacremento'],
+  correctAnswer: 'Sacremento'
+},
+{
+  question: 'What is the capitol of Washington?',
+  answers: ['Seattle','Olympia','Tacoma','Spokane'],
+  correctAnswer: 'Tacoma'
+},
+{
+  question: 'What is the state capitlol of Oregon?',
+  answers: ['Portland', 'Eugene', 'Bend', 'Salem'],
+  correctAnswer: 'Salem'
+},
+{
+  question: 'What is the state capitol of Montana?',
+  answers: ['Bozeman','Helena','Billings','Great Falls'],
+  correctAnswer: 'Helena'
+},
+{
+  question: 'What is the state capitol of Texas?',
+  answers: ['Austin','Dallas','Fort Worth', 'El Paso'],
+  correctAnswer: 'Austin'
+},
+{
+  question: 'What is the state capitol of Idaho?',
+  answers: ['Idaho Falls','Boise','Twin Falls', 'Nampa'],
+  correctAnswer: 'Boise'
+},
+{
+  question: 'What is the state capitol of Kentucky',
+  answers: ['Frankfort','Louisville','Lexington','Bardstown'],
+  correctAnswer: 'Frankfort'
+}
+];
